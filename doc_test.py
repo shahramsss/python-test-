99a7eeeb0bf449178cbe7e5f7515f731bbc16cf6
shah <shahramsss1987@gmail.com>
2022-02-19 09:41:42 +0330

    doc tests added

---
+++ doc_test.py
@@ -0,0 +1,39 @@
+def sum(x, y):
+    """
+    >>> sum(1,2)
+    3
+    """
+    return x+y
+
+
+def subtract(x, y):
+    """
+    >>> subtract(2, 5)
+    -3
+    >>> subtract(7, 5)
+    2
+    """
+    return x-y
+
+
+def multiply(x, y):
+    """
+    >>> multiply(3,5)
+    15
+    >>> multiply(-3,5)
+    -15
+    """
+    return x*y
+
+
+"""
+>>> sum(4,9)
+13
+
+>>> sunbtract(4,9)
+-5
+
+>>> multiplay(4,9)
+36
+
+"""
